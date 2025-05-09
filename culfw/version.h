797e5e60f6ac845ae5954a894a4266e38bc4b630
vbs <364469+verybadsoldier@users.noreply.github.com>
2025-05-09 21:39:08 +0200

    set version to 1.26.09-rc1

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               26
-#define VERSION                 "1.26.09"
+#define VERSION                 "1.26.09-rc1"
 #define BUILD_DATE              "unknown"
 #define BUILD_NUMBER            "private build"
 #define FW_NAME                 "a-culfw"
